8a73fa6e6aa76f1c04b209ae7e498e8f9b7393d6
David Specht <dspecht@gmail.com>
2014-05-01 17:25:33 -0400

    Initial WP Commit

---
+++ wp-content/themes/wpbootstrap/footer.php
@@ -0,0 +1,14 @@
+<hr>
+
+      <footer>
+        <p>© BJ's Produce 2014</p>
+          <?php wp_list_pages(array('title_li' => '', 'include' => '43')); ?>
+
+      </footer>
+
+    </div> <!-- /container -->
+
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
